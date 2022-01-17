b91337d4c3893eaa2ae10c0a9fe5be53eb638a8d
Mike Sanders <msanders331@gmail.com>
2022-01-17 06:42:10 -0800

    Updated the style and html structure at a basic level. The styles will be updated once the JS code has been completed.

--- js/quiz.js
+++ js/quiz.js
@@ -1,10 +1,14 @@
-/*
- * questions.js is loaded in the HTML before quiz.js
- * It creates a global variable called questions that contains starter questions.
- * Take a look at the structure and familiarize yourself with each part
- * then, add some of your own questions!
- * Use this data to populate your quiz questions, choices, and answers.
- */
-console.log(questions);
+//Global Variables
+var totalScore = 0;
+var timerCount = document.querySelector('timer-count');
 
 
+//TODO: Build Start Game Function
+
+//TODO: Build View Highscore Function
+
+//TODO: Build Time Function
+
+//TODO: Build Win Condition
+
+//TODO: Build Function to walk through array and create the boxes.
