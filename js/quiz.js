3471f698f5a59a642e8a906ea2a2a4121f3624c2
Mike Sanders <msanders331@gmail.com>
2022-01-17 09:52:55 -0800

    Added in a timer function to track and count down from 75 seconds which is the given time to answer all of the questions.

--- js/quiz.js
+++ js/quiz.js
@@ -1,10 +1,3 @@
-//Global Variables
-var totalScore = 0;
-var timerCount = document.querySelector('timer-count');
-
-
-//TODO: Build Start Game Function
-
 //TODO: Build View Highscore Function
 
 //TODO: Build Time Function
@@ -12,3 +5,39 @@ var timerCount = document.querySelector('timer-count');
 //TODO: Build Win Condition
 
 //TODO: Build Function to walk through array and create the boxes.
+
+//Global Variables
+var mainHeader = document.querySelector('.main-header');
+var timerEl = document.querySelector('.timer-count');
+var startButton = document.querySelector('.start-game');
+
+var questionsLength = questions.length;
+var totalScore = 0;
+var timer;
+var timerCount;
+var questionIndex;
+
+var startGame = function(){
+    timerCount = 75;
+    //timer starts
+    startTimer();
+    //display question and render button
+
+};
+
+//TODO: Needs win condition & end of quiz conditions added
+var startTimer = function(){
+    //check if the questions have been answered
+    timer = setInterval(function(){
+        timerCount--;
+        timerEl.textContent = timerCount;
+        if(timerCount === 0){
+            clearInterval(timer);
+        }
+
+    }, 1000);
+
+};
+
+//Function called to start the game. Attached to the start quiz button.
+startButton.addEventListener('click', startGame);
